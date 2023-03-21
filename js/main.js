2e72bbea26648761ea55e882c90b7066330c9374
Alexandra <shvets1998@inbox.ru>
2023-03-21 18:07:53 +0300

    Исправления по 8му дз

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 import {createPhotoDescriptionArray} from './data.js';
 const generatedPictureByNumber = createPhotoDescriptionArray();
 import {generatedPicture} from './miniature.js';
-generatedPicture(generatedPictureByNumber);
-
-//import './modal.js';
 import {openBigPicture} from './modal.js';
 openBigPicture (generatedPictureByNumber);
-
+generatedPicture(generatedPictureByNumber);
